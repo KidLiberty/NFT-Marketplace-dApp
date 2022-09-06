e918a1de70b428710a989a1700cc344149ee149c
Brandon Stone <brandonrstone@gmail.com>
2022-09-06 15:24:42 -0400

    Started constructing Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { useRouter } from 'next/router'
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import images from '../assets'
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <nav className='flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1'>
+      <div className='flex flex-1 flex-row justify-start'>
+        <Link href='/'>
+          <div className='flexCenter md:hidden cursor-pointer'>
+            <Image
+              src={images.logo02}
+              objectFit='contain'
+              width={32}
+              height={32}
+              alt='logo'
+            />
+            <p className='dark:text-white text-nft-black-1 font-semibold text-lg ml-2'>
+              OpenLake
+            </p>
+          </div>
+        </Link>
+        <Link href='/'>
+          <div className='hidden md:flex cursor-pointer' onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit='contain'
+              width={32}
+              height={32}
+              alt='logo'
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className='flex flex-initial flex-row justify-end'>
+        <div className='flex items-center mr-2'>
+          <input
+            className='checkbox'
+            id='checkbox'
+            type='checkbox'
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label
+            className='flexBetween w-8  h-4 bg-black rounded-2xl p-1 relative label'
+            htmlFor='checkbox'
+          >
+            <i className='fa fa-sun' />
+            <i className='fa fa-moon' />
+            <div className='w-3 h-3 absolute bg-white rounded-full ball' />
+          </label>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Script from 'next/script'
+import { ThemeProvider } from 'next-themes'
+
+import { Navbar, Footer } from '../components'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute='class'>
+    <div className='dark:bg-nft-dark bg-white min-h-screen'>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script
+      src='https://kit.fontawesome.com/7b553fb718.js'
+      crossorigin='anonymous'
+    />
+  </ThemeProvider>
+)
 
 export default MyApp
